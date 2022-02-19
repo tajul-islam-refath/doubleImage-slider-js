1aea5a1896bea66d8df6535d1b43ee243230c18f
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2022-02-19 21:52:59 +0600

    Update animations code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var slideTime = 5000;
+var slideTime = 8000;
 var slideIndex = -1;
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("slider-inner");
+  var slidesUpperImages = document.getElementsByClassName("slider-inner--img");
   // console.log(slideIndex);
   if (slideIndex < slides.length - 1) {
     slideIndex++;
@@ -13,9 +14,31 @@ function showSlides() {
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slidesUpperImages[i].style.display = "none";
   }
 
   slides[slideIndex].style.display = "block";
+  slides[slideIndex].style.animation = "slideout 4s";
+  slidesUpperImages[slideIndex].style.display = "block";
+  slidesUpperImages[slideIndex].style.animation = "slideout 4s";
+
+  setTimeout(() => {
+    if (slideIndex + 1 <= slides.length - 1) {
+      slides[slideIndex].style.display = "none";
+      slidesUpperImages[slideIndex].style.display = "none";
+      slides[slideIndex + 1].style.display = "block";
+      slides[slideIndex + 1].style.animation = "slidein 4s";
+      slidesUpperImages[slideIndex + 1].style.display = "block";
+      slidesUpperImages[slideIndex + 1].style.animation = "imageSlidin 4s";
+    } else {
+      slides[slides.length - 1].style.display = "none";
+      slidesUpperImages[slides.length - 1].style.display = "none";
+      slides[0].style.display = "block";
+      slides[0].style.animation = "slidein 4s";
+      slidesUpperImages[0].style.display = "block";
+      slidesUpperImages[0].style.animation = "imageSlidin 4s";
+    }
+  }, 4000);
 
   setTimeout(showSlides, slideTime);
 }
